d10a0b87261691b21e35d2103ec7152659c0d053
LaureanoG <40482521+LaureanoG@users.noreply.github.com>
2021-07-14 01:58:48 -0400

    Start off point for website

---
+++ schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS Administrator;
+
+CREATE TABLE Administrator {
+    AdminID int,
+    MenuID int,
+    Fname varchar(50),
+    Lname varchar(50),
+    Username varchar(50),
+    Password varchar(50)
+};
