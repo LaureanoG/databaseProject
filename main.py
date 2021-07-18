3ab97cd5246d77552f91e6a776c481a9dce4d9e6
LaureanoG <40482521+LaureanoG@users.noreply.github.com>
2021-07-18 19:41:48 -0400

    database changes

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
 app = Flask(__name__)
-@app.route('/')
-def index():
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method == "POST":
+        cur = mysql.connection.cursor()
+        file = open('CreateTable.txt', 'r')
+        for i in file.readlines():
+            cur.execute(i)
+
     return render_template('home.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@app.route("/cart", methods=['GET', 'POST'])
+def cart():
+    return render_template("cart.html")
+
+@app.route("/menu", methods=['GET', 'POST'])
+def menu():
+    return render_template("menu.html")
+
