2cd9623f24a5f6f15b0af8f7d859f9c6e8201ebc
LaureanoG <40482521+LaureanoG@users.noreply.github.com>
2021-07-19 01:36:30 -0400

    Login Functionality & Separation of Customer and Admin

--- main.py
+++ main.py
@@ -1,33 +1,81 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
 import sqlite3
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import re
 
 app = Flask(__name__)
-mysql = MySQL(app)
+
+
+# Login info for default admin
+# Username: Admin
+# Password: Password
+# Login info for default customer
+# Username: Customer
+# Password: Password
 
 @app.route('/', methods=['GET', 'POST'])
-
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    if request.method == "POST":
-        cur = mysql.connection.cursor()
-        file = open('CreateTable.txt', 'r')
-        for i in file.readlines():
-            cur.execute(i)
-
-    return render_template('home.html')
+def initialize():
+    connector = sqlite3.connect('database.db')
+    cursor = connector.cursor()
+    file = open('CreateTable.txt', 'r')
+    for i in file.readlines():
+        cursor.execute(i)
+    connector.commit()
+    return render_template("login.html", msg='Welcome to LLN Resto!')
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        connector = sqlite3.connect('database.db')
+        cursor = connector.cursor()
+        if 'Admin' == request.form['login']:
+            cursor.execute('SELECT Password FROM Administrator WHERE Username = (?);', (username, ))
+        if 'Customer' == request.form['login']:
+            cursor.execute('SELECT Password FROM Customer WHERE Username = (?);', (username, ))
+        passwordDb = cursor.fetchone()[0]
+        if password == str(passwordDb):
+            msg = username + 'has logged in successfully'
+            if 'Admin'== request.form['login']:
+                return render_template("homeAdmin.html")
+            if 'Customer' == request.form['login']:
+                return render_template("homeCustomer.html")
+        else:
+              msg = 'The provided credentials do not match our records.\nPlease try again.' + str(passwordDb)
+    else:
+        msg = 'Welcome to LLN Resto!'
+    return render_template("login.html", msg = msg)
+
+@app.route('/homeAdmin', methods=['GET', 'POST'])
+def homeAdmin():
+    return render_template('homeAdmin.html')
+
+@app.route('/homeCustomer', methods=['GET', 'POST'])
+def homeCustomer():
+    return render_template('homeCustomer.html')
+
 
 @app.route("/cart", methods=['GET', 'POST'])
 def cart():
     return render_template("cart.html")
 
-@app.route("/menu", methods=['GET', 'POST'])
-def menu():
-    return render_template("menu.html")
+
+@app.route("/menuCustomer", methods=['GET', 'POST'])
+def menuCustomer():
+    return render_template("menuCustomer.html")
+
+
+@app.route("/menuAdmin", methods=['GET', 'POST'])
+def menuAdmin():
+    return render_template("menuAdmin.html")
+
+@app.route("/orderHistory", methods=['GET', 'POST'])
+def orderHistory():
+    return render_template("orderHistory.html")
+
+
+@app.route("/admin", methods=['GET', 'POST'])
+def admin():
+    return render_template("admin.html")
 
