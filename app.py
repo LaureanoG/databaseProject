bbfd13419a0f34825df28c7a3e47d1b70a77c17a
Skye-NKS <86992962+Skye-NKS@users.noreply.github.com>
2021-07-19 15:41:21 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,99 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.utils import secure_filename
-#from db import db_init, db
-#from models import Img
-
+import sqlite3
 
 app = Flask(__name__)
-app.secret_key = "hello"
-# SQLAlchemy config. Read more: https://flask-sqlalchemy.palletsprojects.com/en/2.x/
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///img.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#db_init(app)
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template("home.html")
 
-@app.route('/menu')
-def menu():
-    return render_template('menu.html')
+# Login info for default admin
+# Username: Admin
+# Password: Password
+# Login info for default customer
+# Username: Customer
+# Password: Password
 
-@app.route('/cart')
-def cart():
-    return render_template('cart.html')
+@app.route('/', methods=['GET', 'POST'])
+def initialize():
+    connector = sqlite3.connect('database.db')
+    cursor = connector.cursor()
+    file = open('CreateTable.txt', 'r')
+    for i in file.readlines():
+        cursor.execute(i)
+    connector.commit()
+    return render_template("login.html", msg='Welcome to LLN Resto!')
 
-@app.route('/login')
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        connector = sqlite3.connect('database.db')
+        cursor = connector.cursor()
+        if 'Admin' == request.form['login']:
+            cursor.execute('SELECT Password FROM Administrator WHERE Username = (?);', (username,))
+        if 'Customer' == request.form['login']:
+            cursor.execute('SELECT Password FROM Customer WHERE Username = (?);', (username,))
+        try:
+            passwordDb = cursor.fetchone()[0]
+        except TypeError:
+            return render_template("login.html", msg="Please fill out the form completely")
+
+        if password == str(passwordDb):
+            msg = username + 'has logged in successfully'
+            if 'Admin' == request.form['login']:
+                return render_template("homeAdmin.html")
+            if 'Customer' == request.form['login']:
+                return render_template("homeCustomer.html")
+        else:
+            msg = 'The provided credentials do not match our records.\nPlease try again.' + str(passwordDb)
+    else:
+        msg = 'Welcome to LLN Resto!'
+    return render_template("login.html", msg=msg)
 
 
+@app.route('/homeAdmin', methods=['GET', 'POST'])
+def homeAdmin():
+    return render_template('homeAdmin.html')
 
-#if __name__ == '__main__':
-#   app.run(debug=True)
+
+@app.route('/homeCustomer', methods=['GET', 'POST'])
+def homeCustomer():
+    return render_template('homeCustomer.html')
+
+
+@app.route("/cart", methods=['GET', 'POST'])
+def cart():
+    return render_template("cart.html")
+
+
+@app.route("/menuCustomer", methods=['GET', 'POST'])
+def menuCustomer():
+    return render_template("menuCustomer.html")
+
+
+@app.route("/menuAdmin", methods=['GET', 'POST'])
+def menuAdmin():
+    return render_template("menuAdmin.html")
+
+
+@app.route("/admin", methods=['GET', 'POST'])
+def admin():
+    return render_template("admin.html")
+
+
+@app.route('/manageCustomers', methods=['GET', 'POST'])
+def manageCustomers():
+    return render_template('manageCustomers.html') 
+
+
+@app.route('/addCustomerForm', methods=['GET', 'POST'])
+def addCustomerForm():
+    return render_template('addCustomerForm.html') 
+
+
+@app.route("/orderHistory", methods=['GET', 'POST'])
+def orderHistory():
+    return render_template("orderHistory.html")
+
