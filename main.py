3c6e5afa358908742bbc52f0cfcff28d20e5591a
LaureanoG <40482521+LaureanoG@users.noreply.github.com>
2021-07-19 01:39:10 -0400

    Error Catching

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def initialize():
     connector.commit()
     return render_template("login.html", msg='Welcome to LLN Resto!')
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     msg = ''
@@ -31,25 +32,31 @@ def login():
         connector = sqlite3.connect('database.db')
         cursor = connector.cursor()
         if 'Admin' == request.form['login']:
-            cursor.execute('SELECT Password FROM Administrator WHERE Username = (?);', (username, ))
+            cursor.execute('SELECT Password FROM Administrator WHERE Username = (?);', (username,))
         if 'Customer' == request.form['login']:
-            cursor.execute('SELECT Password FROM Customer WHERE Username = (?);', (username, ))
-        passwordDb = cursor.fetchone()[0]
+            cursor.execute('SELECT Password FROM Customer WHERE Username = (?);', (username,))
+        try:
+            passwordDb = cursor.fetchone()[0]
+        except TypeError:
+            return render_template("login.html", msg="Please fill out the form completely")
+
         if password == str(passwordDb):
             msg = username + 'has logged in successfully'
-            if 'Admin'== request.form['login']:
+            if 'Admin' == request.form['login']:
                 return render_template("homeAdmin.html")
             if 'Customer' == request.form['login']:
                 return render_template("homeCustomer.html")
         else:
-              msg = 'The provided credentials do not match our records.\nPlease try again.' + str(passwordDb)
+            msg = 'The provided credentials do not match our records.\nPlease try again.' + str(passwordDb)
     else:
         msg = 'Welcome to LLN Resto!'
-    return render_template("login.html", msg = msg)
+    return render_template("login.html", msg=msg)
+
 
 @app.route('/homeAdmin', methods=['GET', 'POST'])
 def homeAdmin():
     return render_template('homeAdmin.html')
+
 
 @app.route('/homeCustomer', methods=['GET', 'POST'])
 def homeCustomer():
@@ -70,6 +77,7 @@ def menuCustomer():
 def menuAdmin():
     return render_template("menuAdmin.html")
 
+
 @app.route("/orderHistory", methods=['GET', 'POST'])
 def orderHistory():
     return render_template("orderHistory.html")
@@ -78,4 +86,3 @@ def orderHistory():
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
     return render_template("admin.html")
-
