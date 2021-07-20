d22280610b6ff534f7317d6ae7c41b371b346297
LaureanoG <40482521+LaureanoG@users.noreply.github.com>
2021-07-19 23:59:12 -0400

    Customer Add & Delete Completed
    Admin side is done!

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_sqlalchemy import SQLAlchemy
+from random import seed, randint
+import time
 import sqlite3
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app = Flask(__name__)
 # Login info for default admin
 # Username: Admin
 # Password: Password
+
 # Login info for default customer
 # Username: Customer
 # Password: Password
@@ -26,7 +28,7 @@ def initialize():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    if request.method == 'POST' and request.form['username'] != "" and request.form['password'] != "":
         username = request.form['username']
         password = request.form['password']
         connector = sqlite3.connect('database.db')
@@ -47,7 +49,9 @@ def login():
             if 'Customer' == request.form['login']:
                 return render_template("homeCustomer.html")
         else:
-            msg = 'The provided credentials do not match our records.\nPlease try again.' + str(passwordDb)
+            msg = 'The provided credentials do not match our records.\nPlease try again.'
+    elif request.method == "POST":
+        msg = 'Please fill out the form completely'
     else:
         msg = 'Welcome to LLN Resto!'
     return render_template("login.html", msg=msg)
@@ -85,15 +89,47 @@ def admin():
 
 @app.route('/manageCustomers', methods=['GET', 'POST'])
 def manageCustomers():
-    return render_template('manageCustomers.html') 
+    connector = sqlite3.connect('database.db')
+    cursor = connector.cursor()
+    cursor.execute('SELECT * FROM Customer;')
+    #if request.method == "POST" and
+    return render_template('manageCustomers.html', data=cursor)
 
+
+@app.route('/delete/<int:CustomerID>', methods=['GET', 'POST'])
+def deleteCustomer(CustomerID):
+    connector = sqlite3.connect('database.db')
+    cursor = connector.cursor()
+    cursor.execute("DELETE FROM Customer WHERE CustomerID = (?)", (CustomerID, ))
+    connector.commit()
+    cursor.execute('SELECT * FROM Customer;')
+    return render_template('manageCustomers.html', data=cursor)
 
 @app.route('/addCustomerForm', methods=['GET', 'POST'])
 def addCustomerForm():
-    return render_template('addCustomerForm.html') 
+    msg = ''
+    # Form is filled
+    if request.method == "POST" and request.form['Fname'] and request.form['Lname'] and request.form['Address'] and \
+            request.form['City'] and request.form['Username'] and request.form['Password'] and request.form['State'] \
+            and request.form['ZipCode'] and request.form['PhoneNumber'] and request.form['Email']:
+        # Add to customer table with the info requested
+        seed(time.time())
+        connector = sqlite3.connect('database.db')
+        cursor = connector.cursor()
+        cursor.execute('INSERT INTO Customer Values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', \
+        (randint(0, 100000), request.form['Fname'], request.form['Lname'], request.form['Username'], request.form['Password'], \
+         request.form['Address'], request.form['City'], request.form['State'], request.form['ZipCode'], \
+         request.form['Email'], request.form['PhoneNumber']))
+        connector.commit()
+        cursor.execute('SELECT * FROM Customer;')
+        return render_template('manageCustomers.html', data = cursor)
+    elif request.method == "POST":  # Form is not filled
+        msg = 'Please fill out the entire form'
+    else:
+        msg = 'Please enter the following information in the fields below and click Add when done.'
+    return render_template('addCustomerForm.html', msg=msg)
 
 
 @app.route("/orderHistory", methods=['GET', 'POST'])
 def orderHistory():
     return render_template("orderHistory.html")
-
